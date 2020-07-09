50ff5ed68812de10a73d3030a84908674c361aa5
Francisco <fbarba@atsistemas.com>
2020-07-09 13:39:30 +0200

    [ADD] new logs

--- src/Parser/D3FormatterLog.php
+++ src/Parser/D3FormatterLog.php
@@ -11,58 +11,76 @@ use Monolog\Formatter\FormatterInterface;
 class D3FormatterLog implements FormatterInterface
 {
     const TITTLE_LOG = 'Tittle';
+    const TITTLE_LOG_PREPARACION = 'Preparacion';
     const TITTLE_LOG_LLAMADA = 'Llamada';
     const TITTLE_LOG_RESPUESTA = 'Respuesta';
     const TITTLE_LOG_RESPUESTA_BRUTA = 'Respuesta Bruta';
     const TITTLE_LOG_RESPUESTA_ARRAY = 'Respuesta Array';
+    const TITTLE_LOG_ERROR = 'Error';
     const BODY_LOG = 'Body';
+    const DATETIME_LOG = 'datetime';
+    const XML_LOG = 'xml';
+    const SOCKET_LOG = 'socket';
 
     /**
      * Formats a log record.
      *
-     * @param  array $record A record to format
+     * @param array $record A record to format
      * @return mixed The formatted record
      */
     public function format(array $record)
     {
-        $message = json_decode( $record['message'], true );
+        $messages = json_decode($record['message'], true);
 
-        $tittle = $message[self::TITTLE_LOG];
-        $date = $record['datetime']->format( 'y-m-d H:i:s.u' );
+        $logResult = '';
+        if (is_array($messages)) {
+            foreach ($messages as $message) {
+                $tittle = $message[self::TITTLE_LOG];
+                $date = $message[self::DATETIME_LOG];
 
-        // ------ HEADER ------- //
-        $header = $tittle.' D3 '." ($date):\n\n";
+                // ------ HEADER ------- //
+                $header = $tittle . ' D3 ' . " ($date):\n\n";
 
-        // ------ BODY ------- //
-        if( $tittle === self::TITTLE_LOG_LLAMADA){
-            $body = $this->parseElementsD3( $message[self::BODY_LOG] );
-        } elseif ( $tittle === self::TITTLE_LOG_RESPUESTA){
-            $body = $this->parseElementsD3( $message[self::BODY_LOG] );
-        } elseif ( $tittle === self::TITTLE_LOG_RESPUESTA_ARRAY){ob_start();
-            $body = var_export($message[self::BODY_LOG], true);
-        } else {
-            $body = $message[self::BODY_LOG];
+                // ------ BODY ------- //
+                switch ($tittle) {
+                    case self::TITTLE_LOG_PREPARACION:
+                        $body = $message[self::XML_LOG] . "\n";
+                        $body .= $message[self::SOCKET_LOG] . "\n\n";
+                        break;
+                    case self::TITTLE_LOG_LLAMADA:
+                    case self::TITTLE_LOG_RESPUESTA:
+                        $body = $this->parseElementsD3($message[self::BODY_LOG]);
+                        break;
+                    case self::TITTLE_LOG_RESPUESTA_ARRAY:
+                        ob_start();
+                        $body = var_export($message[self::BODY_LOG], true);
+                        break;
+                    default:
+                        $body = $message[self::BODY_LOG];
+                }
+
+                // ------ FOOTER ------- //
+                $footer = $this->endRequest();
+                $logResult .= (
+                    $header .
+                    $body .
+                    $footer
+                );
+            }
         }
 
-        // ------ FOOTER ------- //
-        $footer = $this->endRequest();
-
-        return (
-            $header.
-            $body.
-            $footer
-        );
+        return $logResult;
     }
 
     /**
      * @param $params
      * @return string
      */
-    private function parseElementsD3( $params )
+    private function parseElementsD3($params)
     {
         $result = '';
 
-        if ( is_array( $params ) ){
+        if (is_array($params)) {
             foreach ($params as $param) {
                 if (is_array($param)) {
                     $count = 1;
@@ -72,17 +90,17 @@ class D3FormatterLog implements FormatterInterface
                         if (is_array($element)) {
                             foreach ($element as $key => $val) {
                                 if (is_array($val)) {
-                                    $element[$key] = implode( '\\', $val );
+                                    $element[$key] = implode('\\', $val);
                                 }
                             }
-                            $param[$keyElement] = implode( ' ] ', $element );
+                            $param[$keyElement] = implode(' ] ', $element);
                         } else {
                             $param[$keyElement] = $element;
                         }
-                        $param[$keyElement] = $prettyNumber.$param[$keyElement];
+                        $param[$keyElement] = $prettyNumber . $param[$keyElement];
                         $count++;
                     }
-                    $result .= implode( "\n", $param );
+                    $result .= implode("\n", $param);
                 } else {
                     $result .= ' - ' . $param;
                 }
@@ -98,13 +116,13 @@ class D3FormatterLog implements FormatterInterface
      */
     private function endRequest()
     {
-        return ( "\n".str_repeat('-',10)."\n" );
+        return ("\n" . str_repeat('-', 10) . "\n");
     }
 
     /**
      * Formats a set of log records.
      *
-     * @param  array $records A set of records to format
+     * @param array $records A set of records to format
      * @return mixed The formatted set of records
      */
     public function formatBatch(array $records)
